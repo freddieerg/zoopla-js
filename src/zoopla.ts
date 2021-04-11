185e9086e2c4e8fa44221d5a5f1fef6c58bfd1df
Freddie <freddie.erg@outlook.com>
2021-04-11 19:16:37 +0100

    Updated Bridge endpoint

--- src/zoopla.ts
+++ src/zoopla.ts
@@ -8,7 +8,7 @@ class Zoopla {
     private readonly bridgeUrl: string;
 
     constructor() {
-        this.bridgeUrl = 'https://5wi3xv205c.execute-api.eu-west-2.amazonaws.com/';
+        this.bridgeUrl = 'https://p6otn1p763.execute-api.eu-west-2.amazonaws.com/';
     }
 
     /**
